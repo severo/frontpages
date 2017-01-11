f14ccaaebb001b5823f9c2ba84b88f2b2c57c8e9
Sylvain Lesage <severo@rednegra.net>
2017-01-10 22:15:19 -0400

    Détails pour faire marcher le téléchargement des pages

--- README.md
+++ README.md
@@ -6,8 +6,7 @@ Analyser les "unes" en ligne des principaux quotidiens français.
 
 - [x] copie d'écran, sur toute la hauteur de la page, plusieurs fois par jour
 - [x] publier les données en ligne -> https://unes.rednegra.net
+- [x] téléchargement de la page, avec toutes ses ressources
 - [ ] fuseau horaire français
-- [ ] téléchargement de la page, seulement HTML
-- [ ] téléchargement de la page, avec toutes ses ressources
 - [ ] penser aux analyses possibles. Pour tous les candidats à la présidentielle: nombre d'occurrences dans la page, position des occurrences, taille de la police, ou taille du cadre de la mention, de chaque occurrence + présence d'une photo ou non, superficie / carte de la page dédiée à chaque candidat
 - [ ] Utiliser Selenium ? http://www.test-recette.fr/recette/tests-fonctionnels-automatises/testing-automatise-selenium-python.html, http://stackoverflow.com/a/13960516/7351594
--- unes_html.sh
+++ unes_html.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 # Configuration (plus tard : lire le fichier config.ini)
-declare -a sites=("www.liberation.fr" "www.lefigaro.fr")
-baseUrl=/tmp
+declare -a sites=("www.liberation.fr" "www.lefigaro.fr" "www.lemonde.fr" "www.liberation.fr/france" "www.lefigaro.fr/politique/" "lemonde.fr/politique/")
+baseUrl=/var/www/unes
 
 # Telecharger l'url
 function downloadUrl {
   # Parameter 1: url
   # Parameter 2: siteDir
 
-  mkdir -p $siteDir
+  mkdir -p $2
   date=`date +%Y%m%d%H%M%S`
-  downloadDir="$siteDir/$date"
+  downloadDir="$2/$date"
 
   # -E: Append .html to the file name if it is an HTML file but doesn't end in .html or similar
   # -H: Download files from other hosts, too
   # -k: After downloading convert any link in it so they point to the downloaded files
   # -p: Download anything the page needs for proper offline viewing
-  # -r: Download internal files recursively
   # -e robots=off: Ignore robot files
   # -P: save to this directory
-  cmd="wget -E -H -k -p -nd -r -l 10 -e robots=off -P $downloadDir $1"
-  echo cmd
+  cmd="wget -E -H -k -p -nd -l 10 -e robots=off -P $downloadDir $1"
+  echo $cmd
   eval $cmd;
 }
 
 # Boucle sur les journaux
 for site in "${sites[@]}"
 do
-  echo Download "$site"
-  downloadUrl $site "$baseUrl/$site"
+  dir=${site//\//_}
+  echo Download "$site" in $dir
+  downloadUrl $site "$baseUrl/$dir"
 done
